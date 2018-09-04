0ba05aaf0530bd8ee5ebd79e60bd974cf31ee8b3
RamonRomeroTec <A01700318@itesm.mx>
2018-09-04 00:35:36 -0500

    ok

--- src/lab24/cliente/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '<h1>Laboratorio 24: Servicios web REST con php</h1><h2>Acceso</h2><form action="/lab24/cliente/welcome.php" method="POST" >
-  Nombre:<br>
-  <input type="text" name="name" required>
-  
-  <br><br>
-  <input type="submit" value="acceso">
-</form> ';
-?>
